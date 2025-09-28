0cf12e78d17c6a8b715c06f96060a12fa20d8f11
Zulfatun Nikmah <zulfanikmah.id@gmail.com>
2025-09-28 20:04:45 +0700

    loading

---
+++ src/app/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <p className="text-lg animate-pulse text-gray-600">Loading...</p>
+    </div>
+  );
+}
